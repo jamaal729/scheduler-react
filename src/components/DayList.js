80004e55e8a5cab67628ed04b9c41a5c1a306533
Jamaal Ahmed <jamaal729@yahoo.com>
2023-08-05 08:27:07 -0600

    Refactor DayList and InterviewerList to use controlled components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,13 +5,13 @@ import DayListItem from "components/DayListItem.js";
 export default function DayList(props) {
 
   const days = props.days.map(day => {
-    
     return (
       <DayListItem
+        key={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.day}
-        setDay={() => props.setDay(day.name)}
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />
     );
   });
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,15 +4,16 @@ import InterviewerListItem from "components/InterviewerListItem.js";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-
+  const value = props.value;
+  const onChange = props.onChange;
   const interviewers = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === props.interviewer}
-        setInterviewer={() => props.setInterviewer(interviewer.id)}
+        selected={interviewer.id === value}
+        setInterviewer={() => onChange(interviewer.id)}
       />
     );
   });
