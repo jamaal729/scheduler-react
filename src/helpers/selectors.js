59fb361944ef8dc6884e7dea59afdbd03e988dad
Jamaal Ahmed <jamaal729@yahoo.com>
2023-08-12 16:12:19 -0600

    Add Interviewers selector to Form component

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,21 +1,34 @@
 export const getAppointmentsForDay = (state, day) => {
-
-  let currentAppointmentIds = [];
-
+  let appointmentIds = [];
   state.days.filter(d => {
     if (d.name === day) {
-      currentAppointmentIds = d.appointments
+      appointmentIds = d.appointments
     }
   });
-
   let appointmentsForDay = []
-  currentAppointmentIds.forEach(c => {
+  appointmentIds.forEach(c => {
     appointmentsForDay.push(state.appointments[c.toString()]);
   })
-
   return appointmentsForDay;
 };
 
+export const getInterviewersForDay = (state, day) => {
+  let currentDay = null;
+  let currentInterviewers = [];
+  const interviewers = [];
+  state.days.map(d => {
+    if (d.name === day) {
+      currentDay = d.name;
+      currentInterviewers = d.interviewers || [];
+    }
+  });
+  if (currentDay !== null) {
+    for (let i of currentInterviewers) {
+      interviewers.push(state.interviewers[i]);
+    }
+  }
+  return interviewers;
+};
 
 export const getInterview = (state, stateInterview) => {
   if (stateInterview === null) {
