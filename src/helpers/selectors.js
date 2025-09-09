db5c0885a67050de0e4c7b9b106db6262670fc8d
Jamaal Ahmed <jamaal729@yahoo.com>
2025-09-08 18:03:56 -0600

    Transform Appointment data using selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export const getAppointmentsForDay = (state, day) => {
+
+  let currentAppointmentIds = [];
+  state.days.filter(d => {
+    if (d.name === day) {
+      currentAppointmentIds = d.appointments
+    }
+  });
+
+  let appointmentsForDay = []
+  currentAppointmentIds.forEach(c => {
+    appointmentsForDay.push(state.appointments[c.toString()]);
+  })
+
+  return appointmentsForDay;
+};
