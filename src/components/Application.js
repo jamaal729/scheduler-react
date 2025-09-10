2a074b2d909ead0bb4dde8cc5c045a8d91e47cf7
Jamaal Ahmed <jamaal729@yahoo.com>
2025-09-10 08:22:29 -0600

    Combine all Application states

--- src/components/Application.js
+++ src/components/Application.js
@@ -34,8 +34,14 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState(0);
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days, prev) => setState({ ...prev, days });
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days")
@@ -55,7 +61,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
