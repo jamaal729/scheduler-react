0ce23094381555121446e9eddcdf5b6258d6b089
Jamaal Ahmed <jamaal729@yahoo.com>
2025-09-12 12:41:45 -0600

    Retrieve appointments using Axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,54 +6,39 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const appointments = [
-  {
-    id: 1, time: "12pm",
-  },
-  {
-    id: 2, time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: { id: 3, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png", }
-    }
-  },
-  {
-    id: 3, time: "2pm",
-  },
-  {
-    id: 4, time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg", }
-    }
-  },
-  {
-    id: 5, time: "4pm",
-  }
-];
-
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {}
+    appointments: {}
   });
 
+  const dailyAppointments = [];
+
   const setDay = day => setState({ ...state, day });
-  const setDays = (days, prev) => setState({ ...prev, days });
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days")
-      .then(response => {
-        setDays(response.data);
-      });
+    Promise.all([
+      Promise.resolve(axios.get("http://localhost:8001/api/days")),
+      Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
+      Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
+    ]).then(all => {
+      console.log(all[0]); console.log(all[1]); console.log(all[2]);
+      const [days, appointments, interviewers] = all;
+      console.log(days, appointments, interviewers);
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }));
+    })
   }, []);
 
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
@@ -70,7 +55,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(appointment => (
+        {dailyAppointments.map(appointment => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         <Appointment key="last" time="5pm" />
