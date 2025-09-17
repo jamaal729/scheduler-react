bb23d7c25942a4e0885af775d3463b669c98a444
Jamaal Ahmed <jamaal729@yahoo.com>
2025-09-16 21:02:25 -0600

    Use custom hook to render Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,19 +5,31 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "components/Appointment/Form";
+
+import useVisualMode from "hooks/useVisualMode";
+
+const SHOW = "SHOW";
+const EMPTY = "EMPTY";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
-        (<Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        />)
-        :
-        (<Empty />)}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   )
 }
