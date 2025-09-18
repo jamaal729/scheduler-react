8bd420273e494825d9838300db82191e3c14a482
Jamaal Ahmed <jamaal729@yahoo.com>
2025-09-17 20:59:28 -0600

    Add creating appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -25,6 +25,21 @@ export default function Application(props) {
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
+    function bookInterview(id, interview) {
+      console.log(id, interview);
+      const appointment = {
+        ...state.appointments[id],
+        interview: { ...interview }
+      };
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+      // setState({ ...state, appointments });
+      return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+        .then(() => { setState({ ...state, appointments }); });
+    }
+
     return (
       <Appointment
         key={appointment.id}
@@ -32,6 +47,7 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={dailyInterviewers}
+        bookInterview={bookInterview}
       />
     );
   });
@@ -41,17 +57,18 @@ export default function Application(props) {
       Promise.resolve(axios.get("http://localhost:8001/api/days")),
       Promise.resolve(axios.get("http://localhost:8001/api/appointments")),
       Promise.resolve(axios.get("http://localhost:8001/api/interviewers"))
-    ]).then(all => {
-      console.log(all[0]); console.log(all[1]); console.log(all[2]);
-      const [days, appointments, interviewers] = all;
-      console.log(days, appointments, interviewers);
-      setState(prev => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }));
-    })
+    ])
+      .then(all => {
+        console.log(all[0]); console.log(all[1]); console.log(all[2]);
+        const [days, appointments, interviewers] = all;
+        console.log(days, appointments, interviewers);
+        setState(prev => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }));
+      })
   }, []);
 
   return (
